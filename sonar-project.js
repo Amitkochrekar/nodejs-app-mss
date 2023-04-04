77e58cc29eea599e9395925bafc77e305f955ca9
Amitkochrekar <123859808+Amitkochrekar@users.noreply.github.com>
2023-04-04 10:22:51 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.15.34:9000/',
+  serverUrl: 'http://13.127.125.220:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_d74d84c9e6c6ac05bd506d00fb66a90ffbc46547',
+	    'sonar.login': 'squ_307d8e72345da706ee2bd6cf23d7f422a38e61b2',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
